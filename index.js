a2ec89b892e520f6611f35f7bd5f1f26a7c8a4ec
Lae_ <laealt2@gmail.com>
2024-07-09 14:03:47 +0900

    chore: Update launcher.css and index.js
    - Adjust width of #landingPlayerLabel.bot_label in launcher.css
    - Adjust width of #server_status_wrapper in launcher.css
    - Adjust height of .bot_divider in launcher.css
    - Adjust width and position of #mojangStatusWrapper in launcher.css
    - Adjust width and height of BrowserWindow in index.js

--- index.js
+++ index.js
@@ -228,8 +228,8 @@ let win
 function createWindow() {
 
     win = new BrowserWindow({
-        width: 980,
-        height: 552,
+        width: 1300,
+        height: 625,
         icon: getPlatformIcon('SealCircle'),
         frame: false,
         webPreferences: {
